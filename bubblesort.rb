fe5d09cb08562d86edc3f62bf898d340082e97b2
amira <amiraabouhadid@gmail.com>
2021-03-16 18:04:15 +0200

    fix bubble_sort and bubble_sort_by functions

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,13 +1,13 @@
 def bubble_sort(arr)
-  switched = false
-
-  unless switched
+  loop do
+    switched = false
     (arr.length - 1).times do |i|
       if arr[i] > arr[i + 1]
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         switched = true
       end
     end
+    break unless switched
   end
   arr
 end
@@ -17,18 +17,22 @@ data = [1, 89, 4, 43, 9]
 p bubble_sort(data)
 
 def bubble_sort_by(arr)
-  switched = false
-  unless switched
+  loop do
+    switched = false
     (arr.length - 1).times do |i|
-      if (arr[i].length - arr[i + 1].length).positive?
+      diff = yield(arr[i], arr[i + 1]).to_i
+      if diff.positive?
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         switched = true
       end
     end
+    break unless switched
   end
   arr
 end
 
-data1 = %w[coke me these pepperoni]
+data1 = %w[hiya h hey hello hi]
 
-p bubble_sort_by(data1)
+p bubble_sort_by(data1) { |left, right|
+  left.length <=> right.length
+}
