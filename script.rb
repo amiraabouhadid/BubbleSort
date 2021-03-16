1dd41b7995eb9ca5df79ce098e5191fa7aae1367
amira <amiraabouhadid@gmail.com>
2021-03-16 15:06:45 +0200

    add sortby method

--- script.rb
+++ script.rb
@@ -15,3 +15,23 @@ end
 data = [1, 89, 4, 43, 9]
 
 p bubble_sort(data)
+
+def bubble_sort_by(arr)
+
+  switched = false
+
+  unless switched
+
+    (arr.length-1).times do |i|
+      if arr[i].length - arr[i+1].length > 0
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        switched = true
+      end
+    end
+  end
+  arr
+end
+
+data1=["coke","me","these", 'pepperoni']
+
+p bubble_sort_by(data1)
