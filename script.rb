e802771f77cfc2fdbfb1289b2a194954c6b05749
amira <amiraabouhadid@gmail.com>
2021-03-16 15:49:28 +0200

    remove unnecessary parantheses

--- script.rb
+++ script.rb
@@ -21,8 +21,8 @@ def bubble_sort_by(arr)
   unless switched
     (arr.length - 1).times do |i|
        if (arr[i].length - arr[i + 1].length).positive?
-        (arr[i], arr[i + 1] = arr[i + 1], arr[i];
-        switched = true)
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        switched = true
       end
     end
   end
